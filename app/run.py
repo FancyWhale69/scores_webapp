dc157c85d9d98e3f8a910b947c76d05636defb52
FancyWhale69 <80190998+FancyWhale69@users.noreply.github.com>
2021-07-12 22:33:19 +0300

    Update run.py

--- app/run.py
+++ app/run.py
@@ -1,7 +1,6 @@
 import json
 import pickle
 import numpy as np
-import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import MinMaxScaler
